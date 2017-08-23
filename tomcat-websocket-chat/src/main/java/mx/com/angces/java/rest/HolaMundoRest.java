c4eb3ffc2c84926607288d356165e0232f11155f
angces <cesarzg@gmail.com>
2017-08-23 11:25:25 -0500

    Método GET y POST

--- tomcat-websocket-chat/src/main/java/mx/com/angces/java/rest/HolaMundoRest.java
+++ tomcat-websocket-chat/src/main/java/mx/com/angces/java/rest/HolaMundoRest.java
@@ -1,6 +1,7 @@
 package mx.com.angces.java.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,16 +10,24 @@ import javax.ws.rs.core.MediaType;
 import mx.com.angces.java.DTO.Persona;
  
 @Path("/servicioPersonas/")
-@Produces("application/json")
 public class HolaMundoRest {
  
   public HolaMundoRest() {}
  
   @POST
   @Path("/personas")
+  @Produces("application/json")
   @Consumes(MediaType.APPLICATION_JSON)
-  public void addPersona(Persona persona) {
-   
+  public void metodoPOST(Persona persona) {
+  System.out.println("----------HOLA, este es un método POST----------"); 
   System.out.println(persona.getNombre() + " " + persona.getApellidoPaterno());
+  }
+  
+  @GET
+  @Path("/personas")
+  @Produces("text/html")
+  public String metodoGET() {
+  System.out.println("----------HOLA, este es un método GET----------"); 
+  return "<html lang=\"en\"><body><h1>Hola Mundo!!</h1></body></html>";
   }
 }
