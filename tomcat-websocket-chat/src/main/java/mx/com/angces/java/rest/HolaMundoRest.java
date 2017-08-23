2a0fad96c2527e73c3301309a4a3419a4f6dc534
angces <cesarzg@gmail.com>
2017-08-23 10:06:56 -0500

    Con parametros

--- tomcat-websocket-chat/src/main/java/mx/com/angces/java/rest/HolaMundoRest.java
+++ tomcat-websocket-chat/src/main/java/mx/com/angces/java/rest/HolaMundoRest.java
@@ -2,6 +2,7 @@ package mx.com.angces.java.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
  
 @Path("holamundo")
@@ -11,8 +12,8 @@ public class HolaMundoRest {
  
   @GET
   @Produces("text/html")
-  public String getHtml() {
-    return "<html lang=\"en\"><body><h1>Hola Mundo!!</h1></body></html>";
+  public String getHtml(@PathParam("type") String nombre) {
+    return "<html lang=\"en\"><body><h1>Hola " + nombre + "!!</h1></body></html>";
   }
  
 }
