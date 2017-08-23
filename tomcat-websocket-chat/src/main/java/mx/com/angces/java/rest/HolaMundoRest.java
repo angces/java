4b1b2438bdceabbfabe206dc46d37ae26e6a176c
angces <cesarzg@gmail.com>
2017-08-23 10:29:22 -0500

    Ejemplo json

--- tomcat-websocket-chat/src/main/java/mx/com/angces/java/rest/HolaMundoRest.java
+++ tomcat-websocket-chat/src/main/java/mx/com/angces/java/rest/HolaMundoRest.java
@@ -1,19 +1,24 @@
 package mx.com.angces.java.rest;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import mx.com.angces.java.DTO.Persona;
  
-@Path("holamundo")
+@Path("/servicioPersonas/")
+@Produces("application/json")
 public class HolaMundoRest {
  
   public HolaMundoRest() {}
  
-  @GET
-  @Produces("text/html")
-  public String getHtml(@PathParam("type") String nombre) {
-    return "<html lang=\"en\"><body><h1>Hola " + nombre + "!!</h1></body></html>";
+  @POST
+  @Path("/personas")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public void addPersona(Persona persona) {
+   
+  System.out.println(persona.getNombre() + " " + persona.getApellidoPaterno());
   }
- 
 }
