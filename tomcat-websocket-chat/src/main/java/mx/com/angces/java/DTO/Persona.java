61a5b1adcd05bbd2e698e96a0046102846ae16b2
angces <cesarzg@gmail.com>
2017-08-24 07:22:32 -0500

    con dependencias nuevas

--- tomcat-websocket-chat/src/main/java/mx/com/angces/java/DTO/Persona.java
+++ tomcat-websocket-chat/src/main/java/mx/com/angces/java/DTO/Persona.java
@@ -1,16 +1,12 @@
 package mx.com.angces.java.DTO;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class Persona {
 
-	@JsonProperty
 	private String nombre;
-	
-	@JsonProperty
 	private String apellidoPaterno;
-	
-	@JsonProperty
 	private String apellidoMaterno;
 	
 	public String getNombre() {
